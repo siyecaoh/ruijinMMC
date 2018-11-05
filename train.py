edeb17af2e9ccb12577f671b8456b848d8281b1d
leo_luopy@139.com <lpy1990fff>
2018-11-05 09:11:37 +0800

    lr and op

--- train.py
+++ train.py
@@ -2,6 +2,40 @@ import bilstm_crf_model
 import argparse
 from utils import *
 from keras.callbacks import ModelCheckpoint
+import keras
+
+lr_base = 0.001
+epochs = 250
+lr_power = 0.9
+def lr_scheduler(epoch, mode='adam'):
+    '''if lr_dict.has_key(epoch):
+        lr = lr_dict[epoch]
+        print 'lr: %f' % lr'''
+
+    if mode is 'power_decay':
+        # original lr scheduler
+        lr = lr_base * ((1 - float(epoch) / epochs) ** lr_power)
+    if mode is 'exp_decay':
+        # exponential decay
+        lr = (float(lr_base) ** float(lr_power)) ** float(epoch + 1)
+    # adam default lr
+    if mode is 'adam':
+        lr = 0.001
+
+    if mode is 'progressive_drops':
+        # drops as progression proceeds, good for sgd
+        if epoch > 0.9 * epochs:
+            lr = 0.0001
+        elif epoch > 0.75 * epochs:
+            lr = 0.001
+        elif epoch > 0.5 * epochs:
+            lr = 0.01
+        else:
+            lr = 0.1
+
+    print('lr: %f' % lr)
+    return lr
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='TRAIN')
@@ -25,6 +59,8 @@ if __name__ == '__main__':
     filepath = args.save+'/model.h5'
     
     checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+    scheduler = keras.callbacks.LearningRateScheduler(lr_scheduler)
 
-    model.fit(train_x, train_y,batch_size=args.batch,epochs=args.epoch, validation_data=[test_x, test_y],callbacks=[checkpoint])
+    model.fit(train_x, train_y,batch_size=args.batch,epochs=args.epoch,
+              validation_data=[test_x, test_y],callbacks=[checkpoint,scheduler])
     
